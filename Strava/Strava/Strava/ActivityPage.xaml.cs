25cbaeb49d8805e3ec6d42f2342044b676e76d2f
Matt Dreiss <matt.dreiss@daveramsey.com>
2021-01-04 16:38:11 -0600

    Adding my challenge for strava and airbnb

---
+++ Strava/Strava/Strava/ActivityPage.xaml.cs
@@ -0,0 +1,15 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Strava
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ActivityPage : ContentPage
+    {
+        public ActivityPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
---
+++ airbnb/airbnb/airbnb/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace airbnb
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
